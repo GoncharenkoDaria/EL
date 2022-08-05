cc01feac17723f74b6d785ce6286a47de5612890
GoncharenkoDaria <dariacherniygo@gmail.com>
2022-08-05 22:08:18 +0300

    1.01

---
+++ myfirst.cpp
@@ -0,0 +1,10 @@
+// my first cpp- - выводит сообщение на экран 
+# include <iostream> 
+int main () 
+{ 
+    using namespace std; 
+    cout << "Privet."; 
+    cout << endl ; 
+    cout << " Kak dela?" << endl ; 
+    cin.get();
+    return 0; }
