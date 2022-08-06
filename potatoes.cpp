913dc4e36a302546a539ebba19a40fe288f2d9f2
GoncharenkoDaria <dariacherniygo@gmail.com>
2022-08-06 20:56:24 +0300

    1.01

---
+++ potatoes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (void)   // использует и отображает переменную 
+{ 
+    int kartoshka;
+    kartoshka = 3;
+    cout << "I have "; 
+    cout << kartoshka; 
+    cout << " potatoes. " ; 
+    cout << endl ; 
+    kartoshka = kartoshka - 1; 
+
+cout << " Crunch, crunch. Now I have " << kartoshka << " potatoes." << endl ; 
+return 0;} 
